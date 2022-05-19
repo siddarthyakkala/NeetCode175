84f034565b4f67c9cb8933af003d41669111a23c
siddarthyakkala <96491276+siddarthyakkala@users.noreply.github.com>
2022-05-19 18:00:16 +0530

    Create MaximumSubarray.cpp

---
+++ MaximumSubarray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int meh=0;
+        int msf=INT_MIN;
+        for(int i=0;i<nums.size();i++)
+        {
+            meh=meh+nums[i];
+            if(meh<nums[i])
+            {
+                meh=nums[i];
+            }
+            if(msf<meh)
+                msf=meh;
+        }
+        
+        return msf;
+    }
+};
