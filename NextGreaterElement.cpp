f2ede2edebc8673a5fc175e6b1a04ed8c75b34f8
siddarthyakkala <96491276+siddarthyakkala@users.noreply.github.com>
2022-06-19 11:58:29 +0530

    Update NextGreaterElement.cpp

--- NextGreaterElement.cpp
+++ NextGreaterElement.cpp
@@ -1,3 +1,5 @@
+
+//Leetcode
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
@@ -33,3 +35,43 @@ public:
         return ans;
     }
 };
+
+//Interview Bit
+vector<int> Solution::nextGreater(vector<int> &A) {
+    int n=A.size();
+    vector<int> ans;
+    stack<int> st;
+    for(int i=n-1;i>=0;i--)
+    {
+        if(st.empty())
+        {
+            ans.push_back(-1);
+        }
+        else if(st.size()>0 && A[i]<st.top())
+        {
+            ans.push_back(st.top());
+        }
+        else if(st.size()>0 && A[i]>=st.top())
+        {
+            while(st.size()>0 && A[i]>=st.top())
+            {
+                st.pop();
+            }
+            if(st.empty())
+            {
+                ans.push_back(-1);
+            }
+            else if(st.size()>0 && A[i]<st.top())
+            {
+                ans.push_back(st.top());
+            }
+        }
+        
+        st.push(A[i]);
+    }
+    
+    reverse(ans.begin(),ans.end());
+    
+    return ans;
+}
+
