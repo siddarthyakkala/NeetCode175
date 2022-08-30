ecc03f5c66e327ee33a68d8141ad6144c2f5cfdc
siddarthyakkala <96491276+siddarthyakkala@users.noreply.github.com>
2022-08-30 12:14:23 +0530

    Create FIndAllNumbersDisappeared.java

---
+++ FIndAllNumbersDisappeared.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> li = new ArrayList<Integer>();
+        int idx=-1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<0){
+                idx=nums[i]*-1-1;
+            }
+            else{
+                idx=nums[i]-1;
+            }
+            
+            if(nums[idx]>0){
+                nums[idx]=-nums[idx];
+            }
+        }
+        
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0){
+                li.add(i+1);
+            }
+        }
+        
+        return li;
+    }
+}
